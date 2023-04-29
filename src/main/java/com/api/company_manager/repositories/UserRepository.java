89c49683750940eeff4644dc816d2fe28e6a1c1a
LeonMesquita <leonmesquita16@gmail.com>
2023-04-29 16:44:36 -0300

    feat: create method findByUsername in UserRepository

--- src/main/java/com/api/company_manager/repositories/UserRepository.java
+++ src/main/java/com/api/company_manager/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.api.company_manager.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.api.company_manager.models.UserModel;
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long>{
     
+    Optional<UserModel> findByUsername(String username);
 }
